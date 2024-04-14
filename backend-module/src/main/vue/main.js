616b1990a37bd7931b38aed7212074330a13648f
FITlifeSADGE <lukas8967@gmail.com>
2024-04-14 18:04:36 +0200

    Máme přesměrování

--- backend-module/src/main/vue/main.js
+++ backend-module/src/main/vue/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue';
-//import HelloWorld from './components/HelloWorld.vue'; // Import your HelloWorld component
+import VueRouter from 'vue-router';
 import LoginPage from './components/LoginPage.vue';
+import HomePage from './components/HomePage.vue';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/Home',
+      component: HomePage
+    },
+    {
+      path: '/Home/login',
+      component: LoginPage
+    }
+  ]
+});
 
 new Vue({
   el: '#app',
-  render: h => h(LoginPage) // Render your HelloWorld component directly
+  router,
+  render: h => h('router-view') // Tento řádek říká Vue, aby vykreslovalo komponentu na základě URL adresy.
 });
