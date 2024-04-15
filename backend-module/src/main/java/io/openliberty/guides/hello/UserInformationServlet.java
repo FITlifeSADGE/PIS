db37f7506d41ec4afc7e48c686fe70a43bdcf8da
michalhorejs16 <michalhorejs16@gmail.com>
2024-04-16 01:53:56 +0200

    Removed unused imports

--- backend-module/src/main/java/io/openliberty/guides/hello/UserInformationServlet.java
+++ backend-module/src/main/java/io/openliberty/guides/hello/UserInformationServlet.java
@@ -1,9 +1,7 @@
 package io.openliberty.guides.hello;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
