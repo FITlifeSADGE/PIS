21d4ec9e257408df545d32c2667552b832d02785
Shalatik <simces@email.cz>
2024-04-15 17:03:09 +0200

    fix to /Home/pokus

--- backend-module/src/main/vue/components/LoginPage.vue
+++ backend-module/src/main/vue/components/LoginPage.vue
@@ -71,7 +71,7 @@ methods: {
     .then(response => {
       if (response.ok) {
         console.log('Login successful');
-        this.$router.push('/Home/success');
+        this.$router.push('/Home/pokus');
       } else {
         throw new Error('Login failed');
       }
